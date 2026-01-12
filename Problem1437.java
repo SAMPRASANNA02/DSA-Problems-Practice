06f9af99f578244a5292414cca56e6f6922ad530
Samprasanna S <samshuttle2005@gmail.com>
2026-01-12 22:28:19 +0530

    added

---
+++ Problem1437.java
@@ -0,0 +1,16 @@
+ class Problem1437 {
+    public int tribonacci(int n) {
+        int a = 0, b = 1, c = 1;
+        while (n-- > 0) {
+            int d = a + b + c;
+            a = b;
+            b = c;
+            c = d;
+        }
+        return a;
+        
+    }
+}
+    
+    
+    
