72b5f9e5e17b2af95b7a532957179664e3d54e32
Samprasanna S <samshuttle2005@gmail.com>
2026-02-03 11:37:21 +0530

    added

---
+++ TrappingRainWater.java
@@ -0,0 +1,26 @@
+public class TrappingRainWater  {
+    public int trap(int[] height) {
+
+        int left = 0;
+        int right = height.length - 1;
+        int rightmax = height[right];
+        int leftmax = height[left];
+        int total_water = 0;
+
+        while (left < right ) {
+            if (leftmax < rightmax) {
+                left++;
+                leftmax = Math.max(leftmax, height[left]);
+                total_water += leftmax - height[left];
+            }
+            else {
+                right--;
+                rightmax = Math.max(rightmax, height[right]);
+                total_water += rightmax - height[right];
+            }
+        }
+        return total_water;
+    }
+} {
+    
+}
